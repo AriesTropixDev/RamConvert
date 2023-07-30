6205e825ae0c5b4ae7e14e6e5f9ff80327f5e0f0
AriesTropixDev <63368789+AriesTropixDev@users.noreply.github.com>
2023-07-29 23:34:43 -0500

    Change from ytdl to pytube

--- main.py
+++ main.py
@@ -1,61 +1,43 @@
-from __future__ import unicode_literals
-import youtube_dl
-from termcolor import cprint
-from pyfiglet import figlet_format
-from time import sleep
-import os
-import datetime
+import tkinter
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import *
+from pytube import YouTube
+options = ['144p','240p','360p','480p','720p','1080p']
+root = Tk()
+clicked =tkinter.StringVar(root)
+clicked.set(options[0])
+qual = '144p'
+def extract(s):
+    global qual
+    qual = s
+def Downloader():
+    try:
+     quality = qual
+     url =YouTube(str(link.get()))
 
-now = datetime.datetime.now()
+     video = url.streams.filter( res=f'{quality}').first()
 
-def progress_hook(response):
-    if response["status"] == "finished":
-        file_name = response["filename"]
-        x = open("log.txt", 'a')
-        print("Downloaded " + file_name)
-        x.close()
-    if response["status"] == "finished":
-      file_name = response["filename"]
-      print("Downloaded " + file_name)
-    if response["status"] == 'downloading':
-        x = open("log.txt", 'a')
-        print("Date: ", now.strftime('%m-%d-%Y') ,"Filename:" ,response['filename'], "Progress:" ,response['_percent_str'], "ETA:" ,response['_eta_str'])
-        x.close()
-    if response["status"] == 'downloading':
-      print("Date: ", now.strftime('%m-%d-%Y') ,"Filename:" ,response['filename'], "Progress:" ,response['_percent_str'], "ETA:" ,response['_eta_str'])
+     video.download()
+     Label(root, text = 'DOWNLOADED', font = 'arial 15').grid(row=6,column=2,pady=15)
+    except:
+        messagebox.showerror(title='ERROR!',message='SOMETHING WENT WRONG\n\nTRY AGAIN')
 
 
-clear = lambda: os.system('clear')
+root.geometry('500x300')
+root.resizable(0,0)
+root.config(padx=30,pady=30)
+root.title("youtube video downloader")
+Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').grid(row=0,column=2)
+link = StringVar()
 
-clear()
-cprint(figlet_format("RamConvert.", font="big"), 'blue', attrs=["bold"])
-cprint("Welcome To RamConvert!", "blue", attrs=["bold"])
-print('')
-print(
-    "RamConvert is a YouTube MP3 Audio Downloader that only Requires a YouTube Video URL. RamConvert is powered by Youtube_dl.")
-print('')
-cprint('References:', 'blue', attrs=['bold'])
-print('')
-print('• To See Change History - Go to Changelog.md')
-print('• To See Privacy Policy - Go to Privacy_Policy.txt')
-print('')
-cprint('Announcements', 'blue', attrs=['bold'])
-cprint('RamConvert Will Receive An Update on 02/02/2022', 'red')
-print('')
-sleep(4)
-cprint("Insert the YouTube Video URL", "blue", attrs=['bold'])
-link = input("")
+Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').grid(row=1,column=2)
+link_enter = Entry(root, width = 70,textvariable = link).grid(row=2,column=2,pady=10)
+Label(root,text='Choose the quality',font='arial 15 bold').grid(row=3,column=2)
+quality =ttk.OptionMenu(root,clicked,*options,command=extract).grid(row=4,column=2,pady=10)
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2,
+       command = Downloader).grid(row=5,column=2)
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '360',
-    }],
-    'quiet': True,
-    'progress_hooks': [progress_hook],
-}
 
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([link])
+
+root.mainloop()
